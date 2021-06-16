a382a8063dd7f7bd76c2f923a831b1074f96b6e2
Matvey Mitnitsky <matvey.mitnitsky@logz.io>
2021-06-16 16:31:56 +0300

    Enable substitution for string-utils and fix bug - V5

--- infrastructure/string-utils/src/main/java/io/logz/example/infra/string/utils/Strings.kt
+++ infrastructure/string-utils/src/main/java/io/logz/example/infra/string/utils/Strings.kt
@@ -1,7 +1,13 @@
 package io.logz.example.infra.string.utils
 
+import org.apache.commons.lang3.StringUtils
+
 object Strings {
     fun concat(left: Any, right: Any): String {
-        return "$left $right"
+        return "${strip(left)} ${strip(right)}"
+    }
+
+    private fun strip(obj: Any): String {
+        return StringUtils.strip(obj.toString())
     }
 }
